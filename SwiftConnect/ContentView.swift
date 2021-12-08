8a1504cf0e1c22dd15c98739c195677c1bdee4a1
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-12-08 12:29:34 +1100

    Initial Commit

---
+++ SwiftConnect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftConnect
+//
+//  Created by Wenyu Zhao on 8/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftConnect/SwiftConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConnectApp.swift
+//  SwiftConnect
+//
+//  Created by Wenyu Zhao on 8/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
